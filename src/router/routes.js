d1572097f84320142caa43a7c44d83e6354c4a41
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2020-06-21 19:59:36 +0100

    Home.html in public/pages folder updated and linked. Added some new links to the router.js in the src/router folder

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,9 +14,24 @@ const routes = {
   contact: async () => {
     await loadHTML(path.resolve(__dirname, '../pages') + '/contact.html')
   },
+  invest: async () => {
+    await loadHTML(path.resolve(__dirname, '../pages') + '/invest.html')
+  },
+  loan: async () => {
+    await loadHTML(path.resolve(__dirname, '../pages') + '/loan.html')
+  },
+  faq: async () => {
+    await loadHTML(path.resolve(__dirname, '../pages') + '/faq.html')
+  },
+  privacy: async () => {
+    await loadHTML(path.resolve(__dirname, '../pages') + '/privacy.html')
+  },
+  terms_conditions: async () => {
+    await loadHTML(path.resolve(__dirname, '../pages') + '/terms_conditions.html')
+  },
   '': async () => {
     await loadHTML(path.resolve(__dirname, '../pages') + '/home.html')
   }
 }
 
-export default routes
+export default routes;
